9b6debdc0a1e3df90be9d9e9a597cbde8a87f218
batuhan <batuhan-celebi@hotmail.com>
2024-03-18 01:18:41 +0300

    ejs added and html files has been configured to it. Pages has been torn into partials.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+/*
 app.get("/", (req, res) => {
   const firstObject = {
     id: 1,
@@ -11,7 +14,19 @@ app.get("/", (req, res) => {
   };
   res.status(200).send(firstObject);
 });
-
+*/
+app.get("/", (req, res, next) => {
+  res.render("index.ejs");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post.ejs");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/post", (req, res) => {
+  res.render("/post");
+});
 app.get("*", (req, res) => {
   res.status(404).send("404 No Pages Found");
 });
